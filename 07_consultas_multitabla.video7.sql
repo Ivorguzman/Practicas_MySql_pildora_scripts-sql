c51d713dc62cc504773727750deba22315919779
Ivorguzman <66047248+Ivorguzman@users.noreply.github.com>
2022-10-17 06:04:03 -0400

    respaldo17102022

--- 07_consultas_multitabla.video7.sql
+++ 07_consultas_multitabla.video7.sql
@@ -33,7 +33,7 @@
 -- Requisitos para poder realizar uniones de tablas: 
 -- Ambas tablas deben tener el mismo numero de campos
 --  Los campos debe ser compatibles Ejemplo : Campo Numerico y campo tipo monedad
---  El nombre de los campos que se le asigna a la tabla resultante en la consulta  son los de la primera cunsulta (La primera tabala  en la union de tablas) en el query reslizado
+--  El nombre de los caproductosnuevosmpos que se le asigna a la tabla resultante en la consulta  son los de la primera cunsulta (La primera tabala  en la union de tablas) en el query reslizado
 --  Se pueden utilizar el operador UNION tantas veces que se quiera un una misma consulta
     
           
@@ -69,31 +69,43 @@
 		-- articlos de la tabla de producto cuyo precio sea mayor a 500 y los articulo de la tabla productos nuevos  cuya secion sea alta costura 
 		SELECT  * FROM  productos WHERE  precio >= 500;
         SELECT *  FROM productosnuevos WHERE SECCIÓN='ALTA COSTURA';
-        SELECT  * FROM  productos WHERE  precio >= 500 UNION  SELECT * FROM productosnuevos WHERE SECCIÓN='ALTA COSTURA';
+        SELECT  * FROM  productos WHERE  precio >= 500 UNION  SELECT * FROM productosnuevos WHERE SECCIÓN='ALTA COSTURA' order by precio;
         
         
 		-- articlos de la tabla de producto cuyo precio sea mayor a 500 y los articulo de la tabla productos nuevos  cuya secion sea alta costura ordenadp por precio
-         SELECT  *  FROM  productos WHERE  precio >= 500 UNION  SELECT * FROM productosnuevos WHERE SECCIÓN='ALTA COSTURA'  order by PRECIO ;
+         SELECT  *  FROM  productos WHERE  precio >= 500   UNION  SELECT * FROM productosnuevos WHERE SECCIÓN='ALTA COSTURA'  order by  PRECIO ;
         
-        
-        -- consulta compo de seleccion de la tabla productosnuevos  y compo de seleccion de la tabla productos en una sola tabla de consulta
-         SELECT  SECCIÓN  AS   Secciones_productosnuevos_productos    FROM  productosnuevos ;
-         SELECT   SECCIÓN   FROM  productos;
-         SELECT  SECCIÓN  AS   Secciones_productosnuevos_productos    FROM  productosnuevos  UNION  SELECT   SECCIÓN   FROM productos;
+               
              
+      --  EL OPERADOR  UNION   MUESTRA UNA SOLA VEZ EL A LOS REGISTROS REPETIDOS  EN LA CONSULTA;
+        SELECT  * FROM  productos WHERE SECCIÓN = 'deportes';
+        SELECT * FROM productosnuevos WHERE SECCIÓN='deportes';
+        -- Solo relaciona el registro repetido una sola vez
+        SELECT  * FROM  productos WHERE SECCIÓN = 'deportes' UNION  SELECT * FROM productosnuevos WHERE SECCIÓN='deportes';
+  
              
 		 DESCRIBE  productosnuevos;
 
   
-  
-  
-  
+
+
+
   -- *****************************************   UNION ALL ==>  Permite unir en una unica consulta varias tablas registros repetidos los muestra una sola vez *****************************************
 
+
+  
+    -- Consulta  articulos que pertenescan a Deportes que esta en la base de datos productos   
+  SELECT  * FROM  productos WHERE SECCIÓN = 'deportes';
+  
+  -- consulta de articulo deporte de riesgo  de la seccion deportes de la tabla productosnuevos
+  SELECT * FROM productosnuevos WHERE SECCIÓN='deportes';
   
   
-  
-  
+   --  EL OPERADOR  UNION   MUESTRA UNA SOLA VEZ EL A LOS REGISTROS REPETIDOS  EN LA CONSULTA;
+        SELECT  * FROM  productos WHERE SECCIÓN = 'deportes';
+        SELECT * FROM productosnuevos WHERE SECCIÓN='deportes';
+        --  Realaciona los registros repetidos tantas veces como esten en la consulta
+        SELECT  * FROM  productos WHERE SECCIÓN = 'deportes' UNION ALL SELECT * FROM productosnuevos WHERE SECCIÓN='deportes';
     
     
     
